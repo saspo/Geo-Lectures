648a019a074bfe001bfafc1399d65c4b16d96483
saspo <sina.spors@web.de>
2019-05-16 16:00:09 +0200

    Geysir exercise

--- lecture_5/Lec5_geyserEx (copy).py
+++ lecture_5/Lec5_geyserEx (copy).py
@@ -6,35 +6,15 @@
 # -----------------------------
 import Lec5_geyserEx_functions as h
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 # -- import the snuffler marker file, time in julian days --
-julday, evclass = h.read_snuffler_marker("marker_1605.txt", length=0) #Funktion in extra file
-								      # mit h. aufrufen
-julday = np.sort(julday) #da Marker nicht zeitlich sortiert sind
-plt.plot(julday)
-plt.show()
-
-
+h.read_snuffler_marker
 
 
 # -- calculate the waiting time after eruption/ duration of bursts in minutes --  
 
-#julian day variable
-#calculate the time between two julianday variables 
 
-wait_time = [] 
-
-for i in range (0,len((julday)),1):	
-    #wait_time = np.zeros(julday) #error
-	wait_time.append(julday[i+1]-julday[i]*24*60) #append function
-
-#or 
-#wait_time = np.diff(julday)*24*60
-
-plt.plot(waittingtime)
-plt.show()
 
 # -- calculate mean time --
 
